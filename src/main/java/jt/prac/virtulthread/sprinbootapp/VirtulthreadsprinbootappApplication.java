fabe92c6a2ec54d5da3d735f320b8fd0f9b439b0
Jomy Thomas <jomythomas001@gmail.com>
2026-02-09 12:12:58 +0530

    Spring boot project initialized

---
+++ src/main/java/jt/prac/virtulthread/sprinbootapp/VirtulthreadsprinbootappApplication.java
@@ -0,0 +1,13 @@
+package jt.prac.virtulthread.sprinbootapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtulthreadsprinbootappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirtulthreadsprinbootappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jt/prac/virtulthread/sprinbootapp/VirtulthreadsprinbootappApplicationTests.java
@@ -0,0 +1,13 @@
+package jt.prac.virtulthread.sprinbootapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtulthreadsprinbootappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
